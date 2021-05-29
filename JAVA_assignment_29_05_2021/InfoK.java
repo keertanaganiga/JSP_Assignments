492b362a2e249b13ac97a867218969f085c29ff1
keertanaganiga <43719847+keertanaganiga@users.noreply.github.com>
2021-05-29 09:31:23 -0700

    Add files via upload

---
+++ JAVA_assignment_29_05_2021/InfoK.java
@@ -0,0 +1,40 @@
+package info;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class InfoK {
+
+	public static void main(String[] args) {
+		String databaseURL="jdbc:ucanaccess://Detail.accdb";
+			
+			try {
+				
+				Connection connection=DriverManager.getConnection(databaseURL);
+				Statement statement=connection.createStatement();
+				System.out.println("Connection created");
+				statement.execute("create table Details1(Name varchar,Email varchar,Phone varchar)");
+				System.out.println("Table created");
+				statement.executeUpdate("insert into Details values 'keertana','keertanaganiga@gmail.com','7654342356'");
+				statement.executeUpdate("insert into Details values 'kartik','kartikganiga@gmail.com','9654892356'");
+				System.out.println("Inserted");
+				ResultSet resultset=statement.executeQuery("select * from Details");
+				System.out.println("The details are:");
+				while(resultset.next())
+				{
+					String name=resultset.getString("Name");
+					String email=resultset.getString("Email");
+					String phone=resultset.getString("Phone");
+					System.out.println(name+","+email+","+phone);
+				}
+			}
+			catch(Exception e)
+			{
+				System.out.println(e);
+			}
+		}
+	
+
+}
